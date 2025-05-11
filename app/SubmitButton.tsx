3ad93ad2b8afb92ee89ad7a05ea47eb0ff0136bc
danial137 <danial79fakhrabadi@gmail.com>
2025-05-11 16:51:43 +0330

    Refactor SubmitButton to accept variant prop and update DeleteRoute to use SubmitButton for product deletion

--- app/SubmitButton.tsx
+++ app/SubmitButton.tsx
@@ -3,17 +3,23 @@
 import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button"
 import { Loader2 } from "lucide-react"
-export function SubmitButton({text} : {text: string}) {
+
+interface buttonProps {
+    text: string
+    variant?: "default" | "destructive" | "outline" | "ghost" | "link" | "secondary" | null | undefined
+}
+
+export function SubmitButton({text, variant} : buttonProps) {
     const { pending } = useFormStatus()
     return (
         <>
         {pending ? (
-            <Button disabled>
+            <Button disabled variant={variant}>
                 <Loader2 className="mr-2 h-4 w-4 animate-spin"/>
                 Please Wait
             </Button>
         ) : (
-            <Button type="submit">{text}</Button>
+            <Button variant={variant} type="submit">{text}</Button>
         )}
         </>
     )
--- app/dashboard/products/[id]/delete/page.tsx
+++ app/dashboard/products/[id]/delete/page.tsx
@@ -1,8 +1,9 @@
 import { Card, CardHeader, CardTitle, CardDescription, CardFooter } from "@/components/ui/card"
 import { Button } from "@/components/ui/button"
 import Link from "next/link";
-
-export default function DeleteRoute() {
+import { deleteProduct } from "@/app/actions";
+import { SubmitButton } from "@/app/SubmitButton"
+export default function DeleteRoute({ params }: {params: {id: string} }) {
     return(
         <div className="h-[80vh] w-full flex items-center justify-center">
             <Card className="max-w-xl">
@@ -16,7 +17,10 @@ export default function DeleteRoute() {
                     <Button variant="secondary" asChild>
                         <Link href="/dasboard/products">Cancel</Link>
                         </Button>
-                    <Button variant="destructive">Confirm</Button>
+                        <form action={deleteProduct}>
+                            <input type="hidden" name="productId" value={params.id}/>
+                        <SubmitButton text="Delete Product" variant="destructive"/>
+                        </form>
                  </CardFooter>
             </Card>
         </div>
