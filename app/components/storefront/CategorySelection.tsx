ddf3c4ff4a6709a463cddd42892642fcfd6812b5
danial137 <danial79fakhrabadi@gmail.com>
2025-06-29 17:59:25 +0330

    Enhance CategoriesSelection component with additional product links for Men and Women

--- app/components/storefront/CategorySelection.tsx
+++ app/components/storefront/CategorySelection.tsx
@@ -11,6 +11,9 @@ export function CategoriesSelection() {
                 <h2 className="text-2xl font-extrabold tracking-tight">
                     Shop by Category
                 </h2>
+                <Link className = "text-sm font-semibold text-primary hover:text-primary/80" href="/products/all">
+                    Browse all Products &rarr
+                </Link>
             </div>
 
             <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
@@ -24,6 +27,38 @@ export function CategoriesSelection() {
                     <div className="p-6 flex items-end">
                         <Link href="/products/all">
                             <h3 className="text-white font-semibold">All Products</h3>
+                            <p className="mt-1 text-sm text-white">Shop Now</p>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
+                <div className="group aspect-w-2 aspect-h-1 rounded-xl overflow-hidden sm:aspect-w-1 sm:row-span-2">
+                    <Image
+                        src={men}
+                        alt="All Products Image"
+                        className="object-cover object-center "
+                    />
+                    <div className="bg-gradient-to-b from-transparent to-black opacity-55" />
+                    <div className="p-6 flex items-end">
+                        <Link href="/products/all">
+                            <h3 className="text-white font-semibold">Products for Men</h3>
+                            <p className="mt-1 text-sm text-white">Shop Now</p>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+            <div className="mt-6 grid grid-cols-1 gap-y-6 sm:grid-cols-2 sm:grid-rows-2 sm:gap-x-6 lg:gap-8">
+                <div className="group aspect-w-2 aspect-h-1 rounded-xl overflow-hidden sm:aspect-w-1 sm:row-span-2">
+                    <Image
+                        src={women}
+                        alt="All Products Image"
+                        className="object-cover object-center "
+                    />
+                    <div className="bg-gradient-to-b from-transparent to-black opacity-55" />
+                    <div className="p-6 flex items-end">
+                        <Link href="/products/all">
+                            <h3 className="text-white font-semibold">Products for Women</h3>
                             <p className="mt-1 text-sm text-white">Shop Now</p>
                         </Link>
                     </div>
